b5a4f1398a3966c88766e926a312b7ebb76efac9
wokedenis <goncharov.d150@gmail.com>
2024-07-02 21:54:08 +0300

    please?

--- next.config.js
+++ next.config.js
@@ -9,5 +9,17 @@ module.exports = {
         pathname: '**'
       }
     ]
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+
+      // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped.
+      ...config.resolve.fallback,  
+
+      fs: false, // the solution
+    };
+    
+    return config;
   }
 }
